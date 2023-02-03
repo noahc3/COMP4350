37eb0d20afcb1cf37d0877782db01f2784aeb246
Cody Wallbridge <codywallbridge@gmail.com>
2023-02-03 11:14:14 -0600

    adding tables to migration

--- threadit-api/Migrations/20230202050648_InitialCreate.cs
+++ threadit-api/Migrations/20230202050648_InitialCreate.cs
@@ -39,6 +39,79 @@ namespace ThreaditAPI.Migrations
                 {
                     table.PrimaryKey("PK_UserSessions", x => x.Id);
                 });
+
+            migrationBuilder.CreateTable(
+                name: "UserSettings",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "text", nullable: false),
+                    DarkMode = table.Column<string>(type: "boolean", nullable: false),
+                    ProfilePicture = table.Column<string>(type: "text", nullable: true),
+                    Interests = table.Column<string>(type: "text", nullable: true),
+                    Spins = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserSettings", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Thread",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "text", nullable: false),
+                    Topic = table.Column<string>(type: "text", nullable: false),
+                    OwnerId = table.Column<string>(type: "text", nullable: true),
+                    Title = table.Column<string>(type: "text", nullable: true),
+                    Content = table.Column<string>(type: "text", nullable: true),
+                    DateCreated = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Rips = table.Column<string>(type: "text", nullable: true),
+                    Stitches = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Thread", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Spool",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "text", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Interests = table.Column<string>(type: "text", nullable: true),
+                    Mods = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Spool", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Comment",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "text", nullable: false),
+                    Content = table.Column<string>(type: "text", nullable: false),
+                    OwnerId = table.Column<string>(type: "text", nullable: false),
+                    Edited = table.Column<string>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Comment", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserBelongToSpool",
+                columns: table => new
+                {
+                    uId = table.Column<string>(type: "text", nullable: false),
+                    sId = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserBelongToSpool", x => (x.uId + "" + x.sId));
+                });
         }
 
         /// <inheritdoc />
