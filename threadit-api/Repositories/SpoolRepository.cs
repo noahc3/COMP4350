aefa092d44d71756e86f9dfa12ad703330efaa5a
Cody Wallbridge <codywallbridge@gmail.com>
2023-02-03 20:38:16 -0600

    added repo and service

---
+++ threadit-api/Repositories/SpoolRepository.cs
@@ -0,0 +1,30 @@
+﻿using ThreaditAPI.Database;
+using ThreaditAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ThreaditAPI.Repositories
+{
+    public class SpoolRepository : AbstractRepository
+    {
+        public SpoolRepository(PostgresDbContext dbContext) : base(dbContext)
+        {
+        }
+
+        public async Task<Spool?> GetSpoolAsync(Spool spool)
+        {
+            return await GetSpoolAsync(spool.Id);
+        }
+
+        public async Task<Spool?> GetSpoolAsync(string spoolId)
+        {
+            Spool? dbSpool = await db.Spools.FirstOrDefaultAsync(u => u.Id == spoolId);
+            return dbSpool == default ? null : dbSpool;
+        }
+
+        public async Task InsertSpoolAsync(Spool spool)
+        {
+            await db.Spools.AddAsync(spool);
+            await db.SaveChangesAsync();
+        }
+    }
+}
