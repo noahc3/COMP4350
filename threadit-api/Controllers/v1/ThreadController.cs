a348c86ed1401b10858b38451d759ef909f5a13c
nathanpoppe <nathanpoppe16@gmail.com>
2023-02-13 17:00:19 -0600

    Create thread backend

---
+++ threadit-api/Controllers/v1/ThreadController.cs
@@ -0,0 +1,38 @@
+﻿using System.Net.Cache;
+using ThreaditAPI.Extensions;
+using ThreaditAPI.Middleware;
+using Microsoft.AspNetCore.Mvc;
+using ThreaditAPI.Models;
+using ThreaditAPI.Models.Requests;
+using ThreaditAPI.Services;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace ThreaditAPI.Controllers.v1 {
+    [ApiController]
+    [Route("v1/thread")]
+    public class ThreadController : ControllerBase {
+        [AuthenticationRequired]
+        public async Task<IActionResult> PostThread([FromBody] PostThreadRequest request, [FromServices] ThreadService threadService) {            
+            UserDTO? userDTO = Request.HttpContext.GetUser();
+
+            if (userDTO == null) {
+                return Unauthorized();
+            }
+            
+            Models.Thread thread = new Models.Thread{
+                Title = request.Title,
+                Content = request.Content,
+                Topic = request.Topic,
+                OwnerId = userDTO.Id, 
+                SpoolId = request.SpoolId
+            };
+
+            try {
+                thread = await threadService.InsertThreadAsync(thread);
+                return Ok();
+            } catch (Exception e) {
+                return BadRequest(e.Message);
+            }   
+        }
+    }
+}
