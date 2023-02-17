0e0cb4c801b4dabe622efa1f6db822a3e7821ad4
nathanpoppe <nathanpoppe16@gmail.com>
2023-02-16 21:59:24 -0600

    Add navlink to title and button

--- threadit-web/src/containers/Posts/FeedPostItem.tsx
+++ threadit-web/src/containers/Posts/FeedPostItem.tsx
@@ -2,12 +2,14 @@ import { Box, HStack, VStack, Text, Button, Heading } from "@chakra-ui/react"
 import { observer } from "mobx-react"
 import { FaRegComment } from "react-icons/fa"
 import { IThreadFull } from "../../models/ThreadFull";
+import { NavLink } from "../Router/NavLink";
 import Moment from 'react-moment';
 
-export const FeedPostItem = observer(({thread}: {thread: IThreadFull}) => {
+export const FeedPostItem = observer(({thread}: {thread: IThreadFull | any}) => {
     const dateString = (
         <Moment fromNow>{thread.dateCreated}</Moment>
     )
+
     return (
         <>
             <Box border="1px solid gray" borderRadius="3px" p="2rem" bgColor={"white"} w="100%">
@@ -15,13 +17,17 @@ export const FeedPostItem = observer(({thread}: {thread: IThreadFull}) => {
                     <HStack><Text fontWeight={"bold"}>s/{thread.spoolName}</Text><Text color={"blackAlpha.600"}> • Posted by u/{thread.authorName} • {dateString}</Text></HStack>
                     <HStack>
                         <VStack alignItems="start">
-                            <Heading as='h3' size='md'>
-                                {thread.title}
-                            </Heading>
+                            <NavLink to={"/s/" + thread.spoolName + "/post/" + thread.id}>
+                                <Heading as='h3' size='md'>
+                                    {thread.title}
+                                </Heading>
+                            </NavLink>
                         </VStack>
                     </HStack>
                     <HStack>
-                        <Button leftIcon={<FaRegComment />}>View Comments</Button>
+                        <NavLink to={"/s/" + thread.spoolName + "/post/" + thread.id}>
+                            <Button leftIcon={<FaRegComment />}>View Comments </Button>
+                        </NavLink>
                     </HStack>
                 </VStack>
             </Box>
