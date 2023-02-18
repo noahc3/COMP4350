bc9dc1d3bf681448323c7ef3c5da9dcd826ae977
Cody Wallbridge <codywallbridge@gmail.com>
2023-02-17 23:20:46 -0600

    removed unused definitions for tests

--- threadit-web/src/pages/PostSpool/PostSpool.tsx
+++ threadit-web/src/pages/PostSpool/PostSpool.tsx
@@ -1,12 +1,9 @@
 import React from "react";
-import { Button, Card, CardBody, Flex, FormControl, FormLabel, Input, Stack, Textarea } from "@chakra-ui/react";
+import { Button, Card, CardBody, Flex, FormControl, FormLabel, Input, Stack} from "@chakra-ui/react";
 import { PageLayout } from "../../containers/PageLayout/PageLayout";
 import { navStore } from "../../stores/NavStore";
 import { userStore } from "../../stores/UserStore";
 import SpoolAPI from "../../api/SpoolAPI";
-import { ISpool } from "../../models/Spool";
-import ThreadAPI from "../../api/ThreadAPI";
-import { IUserProfile } from "../../models/UserProfile";
 
 export default function PostSpool() {
     const profile = userStore.userProfile;
