0fd471f090c9262070e1e91a0c269dd446cb8799
Cody Wallbridge <codywallbridge@gmail.com>
2023-02-21 12:27:22 -0600

    updated insertion of user to also add to the UserSettings table. Updated all tests to pass

--- threadit-api/Models/Spool.cs
+++ threadit-api/Models/Spool.cs
@@ -1,13 +1,11 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace ThreaditAPI.Models
+﻿namespace ThreaditAPI.Models
 {
     public class Spool
     {
         public string Id { get; set; } = Guid.NewGuid().ToString();
         public string Name { get; set; } = "";
         public required string OwnerId { get; set; }
-        public List<string> Interests { get; set; } =  new List<string>();
+        public List<string> Interests { get; set; } = new List<string>();
         public List<string> Moderators { get; set; } = new List<string>();
 
 
