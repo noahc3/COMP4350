925addb1702776197e46f620fb1a1ec5e660414c
Noah <noahcuroe@gmail.com>
2023-03-14 01:51:02 -0500

    Finish API work

--- threadit-api-tests/RepositoryTests/CommentRepositoryTests.cs
+++ threadit-api-tests/RepositoryTests/CommentRepositoryTests.cs
@@ -18,57 +18,11 @@ public class CommentRepositoryTests
     }
 
     [Test]
-    public async Task RetrieveComment_NotExists_ShouldFail()
-    {
-        Comment testComment = new Comment()
-        {
-            Id = "64a59d99-c16c-4016-b113-25a2f62cf51f",
-            Content = "Comment Content",
-            OwnerId = "a2k6n2b6-0000-4016-b113-25a2f62cf51f",
-            ThreadId = "823ae618-0b9d-4f35-a3e4-9514b5651dbb",
-            ParentCommentId = null
-        };
-        Comment? returnedComment = await _commentRepository.GetCommentAsync(testComment);
-
-        Assert.That(returnedComment, Is.Null);
-    }
-
-    [Test]
     public async Task RetrieveCommentById_NotExists_ShouldFail()
     {
         Comment? returnedComment = await _commentRepository.GetCommentAsync("bdf89c51-9031-4e9b-b712-6df32cd75641");
 
         Assert.That(returnedComment, Is.Null);
-    }
-
-    [Test]
-    public async Task RetrieveComment_Exists_ShouldPass()
-    {
-        // Create Comment
-        Comment testComment = new Comment()
-        {
-            Id = "64a59d99-c16c-4016-b113-25a2f62cf51f",
-            Content = "Comment Content",
-            OwnerId = "a2k6n2b6-0000-4016-b113-25a2f62cf51f",
-            ThreadId = "823ae618-0b9d-4f35-a3e4-9514b5651dbb",
-            ParentCommentId = null
-        };
-
-        // Ensure Comment is not in database
-        Comment? returnedComment = await _commentRepository.GetCommentAsync(testComment);
-        Assert.That(returnedComment, Is.Null);
-
-        // Add Comment to database
-        await _commentRepository.InsertCommentAsync(testComment);
-        returnedComment = await _commentRepository.GetCommentAsync(testComment);
-
-        // Ensure Comment is added correctly
-        Assert.That(returnedComment, Is.Not.Null);
-        Assert.IsTrue(returnedComment.Id.Equals(testComment.Id));
-        Assert.IsTrue(returnedComment.Content.Equals(testComment.Content));
-        Assert.IsTrue(returnedComment.OwnerId.Equals(testComment.OwnerId));
-        Assert.IsTrue(returnedComment.ThreadId.Equals(testComment.ThreadId));
-        Assert.That(returnedComment.ParentCommentId, Is.Null);
     }
 
     [Test]
